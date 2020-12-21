b3ee84f91160985af2ed5709c4f02082dff00fc8
belcherj <belcherj@gmail.com>
2020-12-21 15:29:10 -0500

    Build: webapp [ci skip]

--- lib/utils/platform.ts
+++
@@ -1,8 +0,0 @@
-// https://github.com/atom/electron/issues/22
-export const isElectron = !!window?.electron;
-
-export const isMac = window?.electron?.isMac;
-
-export const isSafari = /^((?!chrome|android).)*safari/i.test(
-  window.navigator.userAgent
-);
